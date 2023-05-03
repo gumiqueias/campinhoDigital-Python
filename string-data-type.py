6971c1f10c22c46efa625a8f804f01cfdedc0d20
Gustavo Miquéias <73312769+gumiqueias@users.noreply.github.com>
2023-05-03 20:58:48 -0300

    projetos python
    Exercicios realizados em sala de aula durante o bootcamp da Campinho digital

---
+++ string-data-type.py
@@ -0,0 +1,31 @@
+myString = "This is a string."
+
+print(myString)
+
+print(type(myString))
+
+print(myString + " is of the data type " + str(type(myString)))
+
+print ("________________________________________________________")
+
+firstString = "water"
+
+secondString = "fall"
+
+thirdString = firstString + secondString
+
+print(thirdString)
+
+print ("________________________________________________________")
+
+name = input("What is your name? ")
+
+print (name)
+
+print ("________________________________________________________")
+
+color = input("What is your favorite color?  ")
+
+animal = input("What is your favorite animal?  ")
+
+print("{}, you like a {} {}!".format(name,color,animal))
